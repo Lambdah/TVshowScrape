050b131d72c310b4b0c9295c6e3a118f01bc9fe8
Lambdah <eijialford@gmail.com>
2018-06-02 17:41:11 -0700

    Added database in pipeline but still need to process items

--- tvscrap/pipelines.py
+++ tvscrap/pipelines.py
@@ -4,8 +4,22 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import psycopg2
+import databaseNameSpace
 
 class TvscrapPipeline(object):
+    def open_spider(self, spider):
+        hostname = databaseNameSpace.DatabaseName['hostname']
+        username = databaseNameSpace.DatabaseName['username']
+        password = databaseNameSpace.DatabaseName['password']
+        database = databaseNameSpace.DatabaseName['database']
+        self.connection = psycopg2.connect(host=hostname, user=username,
+                                           password=password, dbname=database)
+        self.cur = self.connection.cursor()
+
+    def closer_spider(self, spider):
+        self.cur.close()
+        self.connection.close()
+        
     def process_item(self, item, spider):
         return item
