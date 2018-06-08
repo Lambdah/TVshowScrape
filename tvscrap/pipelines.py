763c40b7cc798b13310e84e0ad992e8b6131d6c3
Lambdah <eijialford@gmail.com>
2018-06-07 23:25:23 -0700

    Added item pipeline to the database

--- tvscrap/pipelines.py
+++ tvscrap/pipelines.py
@@ -16,10 +16,18 @@ class TvscrapPipeline(object):
         self.connection = psycopg2.connect(host=hostname, user=username,
                                            password=password, dbname=database)
         self.cur = self.connection.cursor()
+        self.cur.execute("DELETE FROM tvShowepisodes WHERE network = %s;", (item['tv_network']))
 
     def closer_spider(self, spider):
         self.cur.close()
         self.connection.close()
-        
+
     def process_item(self, item, spider):
+        # You have to import tv_networks database yourself so foreign key works
+        self.cur.execute("INSERT INTO tvShowepisodes(tv_title, tv_intro, tv_name,
+                         tv_link, tv_img, tv_epi_num, network) VALUES (%s, %s, %s, %s, %s, %s, %s);",
+                         (item['tv_title'], item['tv_intro'],item['tv_name'],
+                          item['tv_link'],item['tv_img'],item['tv_episode_number'],
+                          item['tv_network']))
+        self.cur.commit()
         return item
